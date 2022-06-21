77bce2845cfa944a367f32fe469a4073bfd6dd90
voygha <joseluiscastanonalvarado@gmail.com>
2022-06-21 16:40:28 -0500

    Construyendo la interfaz

---
+++ public/assets/js/main.js
@@ -0,0 +1,13 @@
+/* ============================= CHANGE BACKGROUND HEADER ========================== */
+const scrollHeader = () =>
+{
+    const header = document.getElementById('header')
+    this.scrollY >=50 ? header.classList.add('scroll-header')
+                    : header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+/* ============================= sWIPER PRODUCTS========================== */
+
+/* ============================= SHOW SCROLL UP ========================== */
+
+/* ============================= DARK LIGHT THEME ========================== */
---
+++ server.js
@@ -0,0 +1,18 @@
+//load dependenncies
+const express= require('express');
+const  app = express();
+const path = require('path')
+
+//set the engne to ejs
+app.set('views', path.join(__dirname, '/src/views/'));
+
+app.set('view engine', 'ejs');
+
+
+app.use(express.static(__dirname + '/public/assets/img'))
+app.use(express.static(__dirname + '/public/assets/css'))
+
+app.use('/', require('./src/router/router'));
+
+app.listen(8080);
+console.log('8080 is the magic port');
